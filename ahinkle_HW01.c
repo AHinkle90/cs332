76bd060faca48cd73cc599170d1bcc5eb339e501
AHinkle90 <ahinkle@uab.edu>
2026-06-13 23:39:28 +0000

    HW01

--- ahinkle_HW01.c
+++ ahinkle_HW01.c
@@ -102,41 +102,46 @@ void printArray(int arr[], int size)
 
 int main()
 {
-
+    /* sumOfDigits */
     printf("sumOfDigits(123) = %d\n", sumOfDigits(123));
     printf("sumOfDigits(405) = %d\n", sumOfDigits(405));
     printf("sumOfDigits(0) = %d\n", sumOfDigits(0));
     printf("sumOfDigits(7) = %d\n", sumOfDigits(7));
     printf("sumOfDigits(-308) = %d\n\n", sumOfDigits(-308));
 
+    /* UABMaxMinDiff */
     int arr1[] = {3, 7, 2, 9};
     printf("UABMaxMinDiff(arr1) = %d\n", UABMaxMinDiff(arr1, 4));
+    
     int arr2[] = {5, 5, 5, 5, 5, 5};
     printf("UABMaxMinDiff(arr2) = %d\n", UABMaxMinDiff(arr2, 6));
+    
     int arr3[] = {-2, 4, -1, 6, 5};
     printf("UABMaxMinDiff(arr3) = %d\n\n", UABMaxMinDiff(arr3, 5));
 
+    /* replaceEvenWithZero */
     int arr4[] = {1, 2, 3, 4};
     replaceEvenWithZero(arr4, 4);
-    int arr5[] = {2, 4, 6};
-    replaceEvenWithZero(arr5, 3);
-    int arr6[] = {1, 3, 5};
-    replaceEvenWithZero(arr6, 3);
-
     printf("arr4 after replaceEvenWithZero: ");
     printArray(arr4, 4);
 
+    int arr5[] = {2, 4, 6};
+    replaceEvenWithZero(arr5, 3);
     printf("arr5 after replaceEvenWithZero: ");
     printArray(arr5, 3);
 
+    int arr6[] = {1, 3, 5};
+    replaceEvenWithZero(arr6, 3);
     printf("arr6 after replaceEvenWithZero: ");
     printArray(arr6, 3);
 
+    /* perfectSquare */
     printf("\nperfectSquare(16) = %s\n", perfectSquare(16) ? "True" : "False");
     printf("perfectSquare(15) = %s\n", perfectSquare(15) ? "True" : "False");
     printf("perfectSquare(25) = %s\n", perfectSquare(25) ? "True" : "False");
     printf("perfectSquare(36) = %s\n\n", perfectSquare(36) ? "True" : "False");
 
+    /* countVowels */
     printf("countVowels(\"Hello World\") = %d\n", countVowels("Hello World"));
     printf("countVowels(\"UAB CS\") = %d\n", countVowels("UAB CS"));
     printf("countVowels(\"Python\") = %d\n", countVowels("Python"));
